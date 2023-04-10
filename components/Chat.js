ec2a4f09769ae2197f116b7a413cf56b1cfcc1ff
JonathanSeife <112643740+JonathanSeife@users.noreply.github.com>
2023-04-10 09:16:40 -0400

    added firebase

--- components/Chat.js
+++ components/Chat.js
@@ -1,40 +1,111 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation }) => {
-  const [messages, setMessages] = useState([]);
-  const { name } = route.params;
+const firebase = require("firebase");
+require("firebase/firestore");
 
-  useEffect(() => {
-    navigation.setOptions({ title: name });
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
-  }, []);
+export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+      user: {},
+    };
+    const firebaseConfig = {
+      apiKey: "AIzaSyAPhmtiB3eJNueAZ46vEXihQn5uZqMW4Ro",
+      authDomain: "chat-c71a1.firebaseapp.com",
+      projectId: "chat-c71a1",
+      storageBucket: "chat-c71a1.appspot.com",
+      messagingSenderId: "865233048274",
+      appId: "1:865233048274:web:b971eb3e2387c14ae9d30b",
+    };
 
-  const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
+    // Initialize Firebase
+    if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+
+    (this.referenceChatMessages = firebase), firestore().collection("messages");
+  }
+  componentDidMount() {
+    this.referenceChatMessages = firebase.firestore().collection("messages");
+    this.unsubscribe = this.referenceChatMessages.onSnapshot(
+      this.onCollectionUpdate
     );
+
+    this.authUnsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        firebase.auth().signInAnonymously();
+      }
+      this.setState({
+        uid: user.uid,
+        messages: [],
+        user: {
+          _id: user.uid,
+          avatar: user.avatar,
+          name: this.props.route.params.name,
+        },
+        loggedInText: "",
+      });
+
+      this.unsubscribe = this.referenceChatMessages
+        .orderBy("createdAt", "desc")
+        .onSnapshot(this.onCollectionUpdate);
+    });
+  }
+
+  componentWillUnmount() {
+    if (this.referenceChatMessages) {
+      this.unsubscribe();
+      this.authUnsubscribe();
+    }
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    querySnapshot.forEach((doc) => {
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          avatar: data.user.avatar || "",
+          name: data.user.name,
+        },
+      });
+    });
+
+    this.setState({ messages });
   };
 
-  const renderBubble = (props) => {
+  // add message to firestore
+  addMessage = () => {
+    const message = this.state.messages[0];
+    this.referenceChatMessages.add({
+      _id: message._id,
+      createdAt: message.createdAt,
+      image: message.image || null,
+      location: message.location || null,
+      text: message.text || "",
+      uid: this.state.uid,
+      user: message.user,
+    });
+  };
+
+  onSend(messages = []) {
+    this.setState(
+      (previousState) => ({
+        messages: GiftedChat.append(previousState.messages, messages),
+      }),
+      () => {
+        this.addMessage(messages);
+      }
+    );
+  }
+
+  renderBubble(props) {
     return (
       <Bubble
         {...props}
